e6fedb751a303b80ccdc103332a1cb1a43485fe3
MallicTesla <124318078+MallicTesla@users.noreply.github.com>
2023-02-15 18:04:49 -0300

    Ejercicio 03 IMC

---
+++ Ejercicio 03 IMC.py
@@ -0,0 +1,6 @@
+Estatura = float (input("Cuanto medis en M "))
+Peso = float (input("Cuanto pesas en KG "))
+
+IMC = ( Peso / Estatura ** 2 )
+
+print ("Ti indice de maza corporal es de ", round(IMC,2))
