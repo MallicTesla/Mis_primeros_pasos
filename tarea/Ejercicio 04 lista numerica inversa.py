c01079fa06d9bb85567b4efdb0acfae6ab030a7b
MallicTesla <124318078+MallicTesla@users.noreply.github.com>
2023-02-15 23:46:34 -0300

    lo mismo

--- tarea/Ejercicio 04 lista numerica inversa.py
+++ tarea/Ejercicio 04 lista numerica inversa.py
@@ -1,6 +1,10 @@
 for lista in range (100,0, -1 ):
     print (lista )
-    
+
 rango_1_100 = range ( 1 , 101)
 for i in reversed ( rango_1_100 ):
     print(f"{i}")
+
+rango=range(1,101)
+for lista2 in reversed( rango) :
+    print (lista2)
