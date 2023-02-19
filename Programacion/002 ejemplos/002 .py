32ad8bd782be66a6278ee7ffbfea34f6ac8626a5
MallicTesla <124318078+MallicTesla@users.noreply.github.com>
2023-02-19 14:06:38 -0300

    acomode algunas cosas

---
+++ Programacion/002 ejemplos/002 .py
@@ -0,0 +1,16 @@
+try:
+    N1 = int(input ( "poneme el numero que quieras " ))
+except ValueError:
+    print("te dije que me pongas un numero ")
+    
+
+
+try:
+    N2 = int(input ( "otro numero " ))
+
+except ValueError:
+    globals
+    print("te dije que me pongas un numero ")
+
+
+print (N1+N2)
---
+++ Programacion/002 ejemplos/002 suma 2 numeros.py
@@ -0,0 +1,24 @@
+def pedir_numero():
+    while True:
+        try:
+            N1 = int(input ( "poneme el numero que quieras " ))
+            break
+        except ValueError:
+            print("te dije que me pongas un numero ")
+
+
+# una funcion es un bloque de codigo que hace algo  
+# antes de poder usar una funcion ahi que definirla (def)
+# tienen un nombre y pueden ser usadas munchas veses
+# una funcion tambien puede retornar un balor 
+pedir_numero()
+
+        
+while True:
+    try:
+        N2 = int(input ( "poneme el otro numero que quieras " ))
+        break
+    except ValueError:
+        print("te dije que me pongas un numero ")
+
+print (N1+N2)
