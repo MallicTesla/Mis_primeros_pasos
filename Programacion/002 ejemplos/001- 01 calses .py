f3dd7f68e78309e99ee5b7733d483052f13f2a55
MallicTesla <124318078+MallicTesla@users.noreply.github.com>
2023-02-20 02:17:13 -0300

    mejorando cada dia

--- Programacion/002 ejemplos/001- 01 calses .py
+++ Programacion/002 ejemplos/001- 01 calses .py
@@ -1,14 +1,21 @@
 #---------------------clase dinamica----------------------
 class dino:
-    _encendido = True                       #   esto es un metodo "si tiene un _ no se deveria manipular fuera de la clase"
+    _encendido = "primera variavle "        #   esto es un metodo "si tiene un _ no se deveria manipular fuera de la clase"
 
     def apagado(self):                      #   esto es un metodo
         self._encendido = False             #   el self. se usa para hacerle refencia a la variavle dentro de la clase
 
-    def apagado (self):
+    def encendido (self):
         self._encendido = False
 
+d1 = dino()                                 #   instanciar una clase contiene todos los metodos de la clas
+d2 = dino()                                 #   antes de poder llamar un metodo primero ahi que instansear la clase
 
-d1 = dino()                                 #   instanciar una clase contiene todos los metodos de la clase
-d1.apagado()                                #   el . se usa para llamar un metodo dentro de una clase
+print(d1._encendido)
+print(d1._encendido + "1")
+
+d1._encendido = "hola"                      #   el . se usa para llamar un metodo dentro de una clase
+print(d1._encendido)
+
+d1.apagado()
 print(d1._encendido)
