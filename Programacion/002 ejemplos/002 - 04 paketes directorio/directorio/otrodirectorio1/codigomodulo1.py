62a1a80ba4e76f795df1b66d6358db246552bc86
MallicTesla <124318078+MallicTesla@users.noreply.github.com>
2023-02-22 18:13:44 -0300

    paketes de modulos

---
+++ Programacion/002 ejemplos/002 - 04 paketes directorio/directorio/otrodirectorio1/codigomodulo1.py
@@ -0,0 +1,9 @@
+#   los modulos se llaman igual que el fichero
+#   cuando se llama a un modulo se ejecuta tdo lo que esta en el por eso los modulos estan compuestos prinsipal mente por funciones y clases
+
+
+def mas (a , b) :
+    return a + b
+
+def Como_me_llamo () :
+    return __name__
