9f5e6befe6db0e3dc888777fab08d99fb5c12e27
MallicTesla <124318078+MallicTesla@users.noreply.github.com>
2023-04-07 16:54:12 -0300

    se agrego la funcion index en views

--- tarea/Ejercicio 17 Django/catalogo/urls.py
+++ tarea/Ejercicio 17 Django/catalogo/urls.py
@@ -1,1 +1,6 @@
-urlpatterns = []
+from catalogo import views
+from django.urls import path
+
+urlpatterns = [
+    path (r"", views.index , name = "index")
+]
--- tarea/Ejercicio 17 Django/catalogo/views.py
+++ tarea/Ejercicio 17 Django/catalogo/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Director, Genero, Pelicula, Instancia_pelicula
+
+def index (request) :
+    Cantidad_de_peliculas = Pelicula.objects.all().count()
+    Instansias_de_peliculas = Instancia_pelicula.objects.all().count()
+    Cantidad_de_directores = Director.objects.all().count()
+    Peliculas_disponivles = Instancia_pelicula.objects.filter (status__exact = "A").count()
+
+    return render (
+        request,
+        "index.html",
+        context = {
+        "Cantidad de peliculas": Cantidad_de_peliculas,
+        "Instansias de peliculas": Instansias_de_peliculas,
+        "Cantidad de directores": Cantidad_de_directores,
+        "Peliculas disponivles": Peliculas_disponivles,
+        }
+    )
