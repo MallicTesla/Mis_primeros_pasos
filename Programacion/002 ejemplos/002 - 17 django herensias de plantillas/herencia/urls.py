5a0386086a5e1d551c5614d4c4df94144ac9bb5e
Mallic Tesla <el_mallic@hotmail.com>
2023-04-17 14:36:10 -0300

    urls dentro de los html

--- Programacion/002 ejemplos/002 - 17 django herensias de plantillas/herencia/urls.py
+++ Programacion/002 ejemplos/002 - 17 django herensias de plantillas/herencia/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path ("herencia/", views.herencia, name = "herencia"),
     path ("ejemplo/" , views.ejemplo, name = "ejemplo"),
     path ("otro/", views.otro, name = "otro"),
+    # para cuando se avre el servidor aparesca una pagina se coloca la ruta vasia "" 
+    path ("", views.index, name = "index"),
 ]
--- Programacion/002 ejemplos/002 - 17 django herensias de plantillas/herencia/views.py
+++ Programacion/002 ejemplos/002 - 17 django herensias de plantillas/herencia/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
+def index (request) :
+    return render (request, "index.html", {})
+
 def herencia (request) :
-    return render (request, "herencia.html")
+    return render (request, "herencia.html", {})
 
 def ejemplo (request) :
-    return render (request, "ejemplo.html")
+    return render (request, "ejemplo.html", {})
 
 def otro (request) :
-    return render (request, "otro.html")
+    return render (request, "otro.html", {})
