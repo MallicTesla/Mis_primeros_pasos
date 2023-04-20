571ce5a6124677198bcfd5acf53de9b78e06e337
Mallic Tesla <el_mallic@hotmail.com>
2023-04-20 20:59:12 -0300

    cree un modulo en comentarios y la ruta ruta

--- Programacion/002 ejemplos/002 - 14 django/05 django apps/comentarios/models.py
+++ Programacion/002 ejemplos/002 - 14 django/05 django apps/comentarios/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+#   despues de crear o modificar cada modelo (clases) se ejecuta el comando python manage.py makemigrations para ver los cambios u despues se migran para guardarlos
+
+class comments (models.Model) :
+
+    name = models.CharField (max_length = 255, null = False)
+    score = models.IntegerField (default = 3)
+    coment = models.TextField (max_length = 1000, null = True)
+    fecha = models.DateField (null = True)
+    #   si se agrega un campo (columna) nuevo a un codigo existente ay que espesiicar si es null o agregarle un valor por defecto
+    asignatura = models.CharField (max_length = 100, default = "Fiema")
+
+
+    def __str__ (self) :
+        return self.name
