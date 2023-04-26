d86be652a5b08d8630346bd8f9090c70572e5236
Mallic Tesla <el_mallic@hotmail.com>
2023-04-26 16:41:41 -0300

    cree los modelos Autor y Entrada

--- Programacion/002 ejemplos/002 - 14 django/06 django mi blog/post/models.py
+++ Programacion/002 ejemplos/002 - 14 django/06 django mi blog/post/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class Autor (models.Model) :
+    nombre = models.CharField (max_length = 200)
+    email = models.EmailField ()
+
+    def __str__(self) :
+        return self.nombre
+
+class Entrada (models.Model) :
+    #   asi se hace una clabe frania en django 
+    #   para que no se rompa todo cuando se borra un autor se usa on_delete = models.CASCADE que bora todas las entradas de ese autor
+    autor = models.ForeignKey (Autor, on_delete = models.CASCADE)
+    titulo = models.CharField (max_length = 255)
+    cuerpo_texto = models.TextField ()
+    #   la fecha por defecto es la de la puvlicasion
+    fecha_publicasion = models.DateField (default = date.today)
+    reiting = models.IntegerField (default = 5)
+
+    def __str__(self) :
+        return self.titulo
