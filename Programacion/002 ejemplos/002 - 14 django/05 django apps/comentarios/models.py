c9a06274d26ff189953e5009c2d517c8c05cfdf1
Mallic Tesla <el_mallic@hotmail.com>
2023-04-26 14:38:56 -0300

    cree un modelo guardar objeto en la base de datos

--- Programacion/002 ejemplos/002 - 14 django/05 django apps/comentarios/models.py
+++ Programacion/002 ejemplos/002 - 14 django/05 django apps/comentarios/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 #   despues de crear o modificar cada modelo (clases) se ejecuta el comando python manage.py makemigrations para ver los cambios u despues se migran para guardarlos
 
-class comments (models.Model) :
+class Comment (models.Model) :
 
     name = models.CharField (max_length = 255, null = False)
     score = models.IntegerField (default = 3)
-    coment = models.TextField (max_length = 1000, null = True)
+    comment = models.TextField (max_length = 1000, null = True)
     fecha = models.DateField (null = True)
     #   si se agrega un campo (columna) nuevo a un codigo existente ay que espesiicar si es null o agregarle un valor por defecto
     asignatura = models.CharField (max_length = 100, default = "Fiema")
--- Programacion/002 ejemplos/002 - 14 django/05 django apps/comentarios/views.py
+++ Programacion/002 ejemplos/002 - 14 django/05 django apps/comentarios/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Comment
 
 def test (request) :
     return HttpResponse ("Funciono")
+
+def crear (request) :
+    # #   asi creo un registro en la base de datos
+    # comentario = Comment (name = "Pablo", score = 5, comment = "Este es un comentario")
+    # #   y asi se guarda el registro
+    # comentario.save ()
+
+    # tambien se puede hacer asi se crea y guarda el objeto
+    comentario = Comment.objects.create (name = "Mallic", score = 4, comment = "Este es otro comentario")
+
+    return HttpResponse ("Ruta para probar la creacion de modelos y guardar datos en la base de datos")
