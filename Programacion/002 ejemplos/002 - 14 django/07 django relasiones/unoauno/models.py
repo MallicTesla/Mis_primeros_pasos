365fab719ab7d7ff08aa9e231fb7a719b7264e8b
Mallic Tesla <el_mallic@hotmail.com>
2023-04-30 16:49:09 -0300

    relasion uno a uno

--- Programacion/002 ejemplos/002 - 14 django/07 django relasiones/unoauno/models.py
+++ Programacion/002 ejemplos/002 - 14 django/07 django relasiones/unoauno/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Lugar (models.Model) :
+    nombre = models.CharField (max_length = 50)
+    direcsion = models.CharField (max_length = 80)
+
+    def __str__(self) :
+        return self.nombre
+
+class Restaurante (models.Model) :
+    #   cuando se crea una relasion normalmente se creea ek mismo nombre de la clase 
+    #   cuando se usa el modulo de relasionamiento OneToOneField (se le tiene que pasar primero el nombre de la clase con la que se le quiere relasionar
+    #   , que es lo que ocure cuando se borra un dato, y como se va a realisar la relasion)
+    lugar = models.OneToOneField (Lugar, on_delete = models.CASCADE, primary_key = True)
+    numero_empleados = models.IntegerField (default = 1 )
+
+    def __str__(self) :
+        return self.lugar.nombre
--- Programacion/002 ejemplos/002 - 14 django/07 django relasiones/unoauno/views.py
+++ Programacion/002 ejemplos/002 - 14 django/07 django relasiones/unoauno/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Lugar, Restaurante
 
-# Create your views here.
+def crear (request) :
+    lugar = Lugar (nombre = "Lugar 1", direcsion = "Calle demo")
+    lugar.save ()
+
+    restaurante = Restaurante (lugar = lugar, numero_empleados = 8)
+    restaurante.save ()
+    return HttpResponse (restaurante.lugar.nombre)
+
