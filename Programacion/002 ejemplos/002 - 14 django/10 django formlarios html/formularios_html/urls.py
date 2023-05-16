6606454b8b084acae5ea15aca30f6b293b0cf0a9
Mallic Tesla <el_mallic@hotmail.com>
2023-05-16 18:58:44 -0300

    enviar formularios por POST

--- Programacion/002 ejemplos/002 - 14 django/10 django formlarios html/formularios_html/urls.py
+++ Programacion/002 ejemplos/002 - 14 django/10 django formlarios html/formularios_html/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ("form/", views.form, name = "form"),
-    path ("destino/", views.destino, name = "destino"),
+    path ("get/form/", views.getform, name = "form"),
+    path ("get/destino/", views.getdestino, name = "destino"),
+    path ("post/form/", views.postform, name = "postform"),
+    path ("post/destino/", views.postdestino, name = "postdestino"),
 ]
--- Programacion/002 ejemplos/002 - 14 django/10 django formlarios html/formularios_html/views.py
+++ Programacion/002 ejemplos/002 - 14 django/10 django formlarios html/formularios_html/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def form (request) :
+def getform (request) :
     
     return render (request, "form.html", {})
 
-def destino (request) :
+def getdestino (request) :
     #   esto es una extroctura de guarda
     if request.method != "GET" :
         return HttpResponse ("el metodo POST no esta soportado")
 
     nombre = request.GET["nombre"]
 
+    return render (request, "suseso.html", {"nombre": nombre})
+
+def postform (request) :
+    return render (request, "postform.html", {})
+
+def postdestino (request) :
+    if request.method != "POST" :
+        return HttpResponse ("el metodo POST no esta soportado")
+
+    nombre = request.POST["nombre"]
 
     return render (request, "suseso.html", {"nombre": nombre})
