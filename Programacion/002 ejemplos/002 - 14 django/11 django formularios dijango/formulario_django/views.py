95cc0cac121b12d34653a75140543e4fd286a8bf
Mallic Tesla <el_mallic@hotmail.com>
2023-05-17 20:29:42 -0300

    otra forma de mostrar el formulario

--- Programacion/002 ejemplos/002 - 14 django/11 django formularios dijango/formulario_django/views.py
+++ Programacion/002 ejemplos/002 - 14 django/11 django formularios dijango/formulario_django/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 #   se importa el archivo forms
 from .forms import Comentario
 def form (request) :
-    comentario_formulario = Comentario
+    #   ais se aparese la caja de texto rellena por defecto. si aui un initial lo anula 
+    comentario_formulario = Comentario ({"nambre":"Pablo", "url":"www.mallic.com", "comentario":"esto va a funcionar"})
+    #   asi queda el formulario como esta en el archivo forms
+    # comentario_formulario = Comentario
     return render (request, "form.html", {"comentario_formulario":comentario_formulario})
 
 def contenido (request) :
