09d71f73160028a9e0f87319d4f24717a0f898b2
Mallic Tesla <el_mallic@hotmail.com>
2023-05-19 19:31:55 -0300

    mas valaidaciones

--- Programacion/002 ejemplos/002 - 14 django/11 django formularios dijango/formulario_django/views.py
+++ Programacion/002 ejemplos/002 - 14 django/11 django formularios dijango/formulario_django/views.py
@@ -2,9 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 #   se importa el archivo forms
 from .forms import Comentario,ContactoForm
+
 def form (request) :
     #   ais se aparese la caja de texto rellena por defecto. si aui un initial lo anula 
-    comentario_formulario = Comentario ({"nambre":"Pablo", "url":"www.mallic.com", "comentario":"esto va a funcionar"})
+    comentario_formulario = Comentario ({"nombre":"Pablo", "url":"www.mallic.com", "comentario":"esto va a funcionar"})
     #   asi queda el formulario como esta en el archivo forms
     # comentario_formulario = Comentario
     return render (request, "form.html", {"comentario_formulario":comentario_formulario})
@@ -13,8 +14,18 @@ def contenido (request) :
     if request.method != "POST" :
         return HttpResponse ("metodo no permitido")
 
-    return HttpResponse (request.POST["nambre"])
+    return HttpResponse (request.POST["nombre"])
 
 def widget (request) :
-    contacto = ContactoForm
-    return render (request, "widget.html", {"contacto":contacto})
+    if request.method == "GET":
+        contacto = ContactoForm
+        return render (request, "widget.html", {"contacto":contacto})
+
+    if request.method == "POST" :
+        contacto = ContactoForm (request.POST)
+        #   ocure esto cuando todos los datos son corectos
+        if contacto.is_valid () :
+            return HttpResponse ("POST vlido")
+        #   ocure esto cuando todos los datos no son corectos
+        else :
+            return render (request, "widget.html", {"contacto":contacto})
