dde59789f7eb0dcd3296feb3a2c95b3a598150df
Mallic Tesla <el_mallic@hotmail.com>
2023-05-20 18:01:54 -0300

    cree urls y views y el html

--- Programacion/002 ejemplos/002 - 14 django/12 django formularios modelos/empleados/views.py
+++ Programacion/002 ejemplos/002 - 14 django/12 django formularios modelos/empleados/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+#   para mostrar un formulario con modelos se importa la calse del formulario
+from .forms import EmpladosForm
 
-# Create your views here.
+def inicio (request) :
+    form = EmpladosForm ()
+    return render (request, "inicio.html", {"form": form })
