5c404420593ebf128b2b65215da772cbaea8b318
Mallic Tesla <el_mallic@hotmail.com>
2023-05-28 14:50:33 -0300

    aregle el codigo y nueva funsion para separar otra

--- Programacion/002 ejemplos/002 - 14 django/14 django proyecto formulario base/empleados/views.py
+++ Programacion/002 ejemplos/002 - 14 django/14 django proyecto formulario base/empleados/views.py
@@ -14,25 +14,6 @@ def menu_ver (request) :
     ruta = request.path.split("/")[1]
     return render(request, "menu_ver.html", {"ruta":ruta})
 
-def super_creasion (request, resource, objeto_1, objeto_2, objeto_3, ruta) :
-    if ruta == "menu_crear" :
-        if request.method == "POST" or objeto_1.is_valid():
-            objeto_1.save()
-
-        elif request.method == "POST":
-            return render(request, "crear.html", {"ver": objeto_2, "request": request, "resource": resource, "ruta":ruta})
-
-        return render(request, "crear.html", {"ver": objeto_2, "request": request, "resource": resource, "ruta":ruta})
-
-    elif ruta == "menu_ver" :
-        # if request.method == "POST" or objeto_1.is_valid():
-        #     objeto_1.save()
-
-        # elif request.method == "POST":
-        #     return render(request, "ver_form/ver_empleado.html", {"ver": objeto_2, "request": request, "resource": resource, "ruta":ruta, 'objeto_3': objeto_3})
-
-        return render(request, "ver_form/ver_empleado.html", {"ver": objeto_2, "request": request, "resource": resource, "ruta":ruta, 'objeto_3': objeto_3})
-
 def salario (request, resource) :
     """
     cuando se llama a este mentodo crea 2 objetos diferentes y lo debuelve 
@@ -40,44 +21,76 @@ def salario (request, resource) :
     objeto_1 = SalarioForm(request.POST)
     objeto_2 = SalarioForm ()
     objeto_3 = Salario.objects.all()
-    ruta = request.path.split("/")[1]
-    return super_creasion (request, resource, objeto_1, objeto_2, objeto_3, ruta)
+    rutas = request.path.split("/")
+    return separar_rutas (request, resource, objeto_1, objeto_2, objeto_3, rutas)
 
 def trabajo (request, resource) :
     objeto_1 = TrabajoForm (request.POST)
     objeto_2 = TrabajoForm ()
     objeto_3 = Trabajo.objects.all()
-    ruta = request.path.split("/")[1]
-    return super_creasion (request, resource, objeto_1, objeto_2, objeto_3, ruta)
+    rutas = request.path.split("/")
+    return separar_rutas (request, resource, objeto_1, objeto_2, objeto_3, rutas)
     
 def pais (request, resource) :
     objeto_1 = PaisForm (request.POST)
     objeto_2 = PaisForm ()
     objeto_3 = Pais.objects.all()
-    ruta = request.path.split("/")[1]
-    return super_creasion (request, resource, objeto_1, objeto_2, objeto_3, ruta)
+    rutas = request.path.split("/")
+    return separar_rutas (request, resource, objeto_1, objeto_2, objeto_3, rutas)
 
 def departamento (request, resource) :
     objeto_1 = DepartamentoForm (request.POST)
     objeto_2 = DepartamentoForm ()
     objeto_3 = Departamento.objects.all()
-    ruta = request.path.split("/")[1]
-    return super_creasion (request, resource, objeto_1, objeto_2, objeto_3, ruta)
+    rutas = request.path.split("/")
+    return separar_rutas (request, resource, objeto_1, objeto_2, objeto_3, rutas)
 
 def local (request, resource) :
     objeto_1 = LocalForm (request.POST)
     objeto_2 = LocalForm ()
     objeto_3 = Local.objects.all()
-    ruta = request.path.split("/")[1]
-    return super_creasion (request, resource, objeto_1, objeto_2, objeto_3, ruta)
+    rutas = request.path.split("/")
+    return separar_rutas (request, resource, objeto_1, objeto_2, objeto_3, rutas)
 
 def empleado (request, resource) :
     objeto_1 = EmpleadoForm (request.POST)
     objeto_2 = EmpleadoForm ()
     objeto_3 = Empleado.objects.all()
-    ruta = request.path.split("/")[1]
-    return super_creasion (request, resource, objeto_1, objeto_2, objeto_3, ruta)
-    
+    rutas = request.path.split("/")
+    return separar_rutas (request, resource, objeto_1, objeto_2, objeto_3, rutas)
+
+def separar_rutas (request, resource, objeto_1, objeto_2, objeto_3, rutas):
+    ruta = rutas[1]
+    if ruta == "menu_crear" :
+        return super_creasion (request, resource, objeto_1, objeto_2, objeto_3, ruta)
+
+    elif ruta == "menu_ver" :
+        return super_ver (request, resource, objeto_1, objeto_2, objeto_3, rutas)
+
+def super_creasion (request, resource, objeto_1, objeto_2, objeto_3, ruta) :
+    if request.method == "POST" or objeto_1.is_valid():
+        objeto_1.save()
+
+    elif request.method == "POST":
+        return render(request, "crear.html", {"ver": objeto_2, "request": request, "ruta":ruta})
+
+    return render(request, "crear.html", {"ver": objeto_2, "request": request, "ruta":ruta})
+
+def super_ver (request, resource, objeto_1, objeto_2, objeto_3, rutas) :
+    ruta = rutas[1]
+    match rutas[2] :
+        case "empleado" :
+            return render(request, "ver_form/ver_empleado.html", {"request": request, "ruta":ruta, 'objeto_3': objeto_3})
+        case "local" :
+            return render(request, "ver_form/ver_local.html", {"request": request, "ruta":ruta, 'objeto_3': objeto_3})
+        case "departamento" :
+            return render(request, "ver_form/ver_departamento.html", {"request": request, "ruta":ruta, 'objeto_3': objeto_3})
+        case "pais" :
+            return render(request, "ver_form/ver_pais.html", {"request": request, "ruta":ruta, 'objeto_3': objeto_3})
+        case "trabajo" :
+            return render(request, "ver_form/ver_trabajo.html", {"request": request, "ruta":ruta, 'objeto_3': objeto_3})
+        case "salario" :
+            return render(request, "ver_form/ver_salario.html", {"request": request, "ruta":ruta, 'objeto_3': objeto_3})
 
 
 
