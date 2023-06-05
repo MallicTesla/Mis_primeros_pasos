8ae353dc86b3469615b05e9bdc8b838258d2e812
Mallic Tesla <el_mallic@hotmail.com>
2023-06-05 17:37:26 -0300

    aregle las rutas y agrege una ruta nueva

--- Programacion/002 ejemplos/002 - 14 django/14 django proyecto formulario base/empleados/urls.py
+++ Programacion/002 ejemplos/002 - 14 django/14 django proyecto formulario base/empleados/urls.py
@@ -1,17 +1,19 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path("", views.inicio, name="inicio"),
+    path ("", views.inicio, name="inicio"),
 
-    path("menu_crear/", views.menu_crear, name="menu_crear"),
-    path("menu_ver/", views.menu_ver, name="menu_ver"),
+    path ("menu_crear/", views.menu_crear, name="menu_crear"),
+    path ("menu_ver/", views.menu_ver, name="menu_ver"),
 
-    path("<str:resource>/empleado/", views.empleado, name="empleado"),
-    path("<str:resource>/local/", views.local, name="local"),
-    path("<str:resource>/departamento/", views.departamento, name="departamento"),
-    path("<str:resource>/pais/", views.pais, name="pais"),
-    path("<str:resource>/trabajo/", views.trabajo, name="trabajo"),
-    path("<str:resource>/salario/", views.salario, name="salario"),
+    path ("<resource>/empleado/", views.empleado, name="empleado"),
+    path ("<resource>/local/", views.local, name="local"),
+    path ("<resource>/departamento/", views.departamento, name="departamento"),
+    path ("<resource>/pais/", views.pais, name="pais"),
+    path ("<resource>/trabajo/", views.trabajo, name="trabajo"),
+    path ("<resource>/salario/", views.salario, name="salario"),
+
+    re_path (r'^(?P<resource>.+)/editar/(?P<id>\d+)/$', views.editar, name = "editar"),
 ]
 
