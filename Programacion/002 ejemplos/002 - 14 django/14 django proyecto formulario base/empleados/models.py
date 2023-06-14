cb9f87147ad997cb8e31381dcf662500e7c84118
Mallic Tesla <el_mallic@hotmail.com>
2023-06-14 15:56:28 -0300

    cambios en el relacionamiento de los modelos

--- Programacion/002 ejemplos/002 - 14 django/14 django proyecto formulario base/empleados/models.py
+++ Programacion/002 ejemplos/002 - 14 django/14 django proyecto formulario base/empleados/models.py
@@ -1,55 +1,54 @@
 from django.db import models
 
-class Empleado (models.Model) :
-    nombre = models.CharField (max_length = 30)
-    apellido = models.CharField (max_length = 30)
-    ci = models.CharField (max_length = 30)
-    direccion = models.CharField (max_length = 100)
-    email = models.EmailField(max_length = 50)
+class Salario(models.Model):
+    monto_anual = models.IntegerField(default=10000)
+    extra_junio = models.BooleanField(default=True)
+    extra_noviembre = models.BooleanField(default=True)
 
-    def __str__(self) :
-        return f"{self.nombre} {self.apellido}"
+    def __str__(self):
+        return str(self.monto_anual)
 
-class Local (models.Model) :
-    local_nombre = models.CharField (max_length = 50)
-    direccion = models.CharField (max_length = 50)
+class Trabajo(models.Model):
+    titulo = models.CharField(max_length=50)
+    descripcion = models.TextField()
 
-    empleado = models.ForeignKey (Empleado, on_delete = models.SET_NULL, null=True)
+    salario = models.ForeignKey(Salario, on_delete=models.SET_NULL, null=True)
 
-    def __str__(self) :
-        return self.local_nombre
-
-class Departamento (models.Model) :
-    departamento = models.CharField (max_length = 30)
-
-    local = models.ForeignKey (Local, on_delete = models.SET_NULL, null=True)
-
-    def __str__(self) :
-        return self.departamento
-
-class Pais (models.Model) :
-    nombre = models.CharField (max_length = 30)
-
-    departamento = models.ForeignKey (Departamento, on_delete = models.SET_NULL, null=True)
-
-    def __str__(self) :
-        return self.nombre
-
-class Trabajo (models.Model) :
-    titulo = models.CharField (max_length = 50)
-    descripsion = models.TextField ()
-
-    empleado = models.ForeignKey (Empleado, on_delete = models.SET_NULL, null=True)
-
-    def __str__(self) :
+    def __str__(self):
         return self.titulo
 
-class Salario (models.Model) :  
-    monto_anual = models.IntegerField (default = 10000)
-    extra_junio = models.BooleanField (default = True)
-    extra_nobiembre = models.BooleanField (default = True)
+class Pais(models.Model):
+    nombre = models.CharField(max_length=30)
 
-    trabajo = models.ForeignKey (Trabajo, on_delete = models.SET_NULL, null=True)
+    def __str__(self):
+        return self.nombre
 
-    def __str__(self) :
-        return self.monto_anual
+class Departamento(models.Model):
+    departamento = models.CharField(max_length=30)
+
+    pais = models.ForeignKey(Pais, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.departamento
+
+class Local(models.Model):
+    local_nombre = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=50)
+
+    departamento = models.ForeignKey(Departamento, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.local_nombre
+
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    ci = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50)
+
+    trabajo = models.ForeignKey(Trabajo, on_delete=models.SET_NULL, null=True)
+    local = models.ForeignKey(Local, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
