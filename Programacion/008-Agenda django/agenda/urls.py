bd1061e446b52ed121c33afa005dc482b632852c
Mallic Tesla <el_mallic@hotmail.com>
2023-06-23 18:41:10 -0300

    creando rutas y asegurandome que funciona

--- Programacion/008-Agenda django/agenda/urls.py
+++ Programacion/008-Agenda django/agenda/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ("agenda/", include ("contactos.urls")),
-    path ("tareas/", include ("tareas.urls"))
+    path ("tareas/", include ("tareas.urls")),
 ]
--- Programacion/008-Agenda django/contactos/views.py
+++ Programacion/008-Agenda django/contactos/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+def inicio (request:HttpRequest):
+    return HttpResponse ("app contactos")
