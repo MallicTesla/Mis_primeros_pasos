86cb6a1fa89a30f8efcd0c3356251d3f3ca3597e
Mallic Tesla <el_mallic@hotmail.com>
2023-07-04 18:14:10 -0300

    crear fomulario

--- Programacion/008-Agenda django/tareas/views.py
+++ Programacion/008-Agenda django/tareas/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from .models import Tarea
+from .forms import TareaForm
 
 def inicio (request:HttpRequest):
     return render (request, "menu/tareas/menu.html", {})
 
 def crear_tarea (request:HttpRequest):
-    return
+    objeto_1 = TareaForm (request.POST)
+    objeto_2 = TareaForm ()
+
+    if request.method == "POST" and objeto_1.is_valid():
+        objeto_1.save()
+
+    return render (request, "tarea/form_tarea.html", {"ver": objeto_2})
