1bd443e004bc7418d5049eb1228aca6d684feba3
Mallic Tesla <el_mallic@hotmail.com>
2023-07-22 17:50:02 -0300

    base para que todos la usen

--- Programacion/008-Agenda django/agenda/settings.py
+++ Programacion/008-Agenda django/agenda/settings.py
@@ -75,24 +75,24 @@ WSGI_APPLICATION = 'agenda.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# mi base de posgres si queres usar la tulla camba el NAME , USER , PASSWOED
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'agena_tareas',
-        'USER': 'mallic',
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# mi base de posgres si queres usar la tulla camba el NAME , USER , PASSWOED
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'agena_tareas',
+#         'USER': 'mallic',
+#         'PASSWORD': '1234',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
