ede876daae2e62497243973b45dec06bd852f206
Mallic Tesla <el_mallic@hotmail.com>
2023-07-22 17:47:28 -0300

    le agrege la parte de administracion

--- Programacion/008-Agenda django/contactos/admin.py
+++ Programacion/008-Agenda django/contactos/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Contacto, Grupo
 
-# Register your models here.
+class ContactoAdmin (admin.ModelAdmin):
+    list_display = ("nombre", "apellido",)
+    search_fields = ("nombre", "apellido",)
+    list_filter = ("nombre","fecha_registro",)
+    date_hierarchy = "fecha_registro"
+
+admin.site.register(Contacto,ContactoAdmin)
+
+class GrupoAdmin (admin.ModelAdmin):
+    list_display = ("nombre",)
+    search_fields = ("nombre",)
+
+    #   Esto agregará la opción de seleccionar contactos al editar un grupo
+    filter_horizontal = ("contactos",)
+
+admin.site.register(Grupo,GrupoAdmin)
--- Programacion/008-Agenda django/tareas/admin.py
+++ Programacion/008-Agenda django/tareas/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Tarea
 
-# Register your models here.
+class TareaAdmin (admin.ModelAdmin):
+    list_display = ("titulo", "breve_descripcion",)
+    search_fields = ("titulo", "breve_descripcion",)
+    list_filter = ("titulo","fecha_registro",)
+    date_hierarchy = "fecha_registro"
+
+admin.site.register(Tarea,TareaAdmin)
