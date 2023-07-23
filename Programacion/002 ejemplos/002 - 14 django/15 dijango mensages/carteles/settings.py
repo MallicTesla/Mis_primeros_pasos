923c6b2af58963cceddfc166b324ff823b1fa32d
Mallic Tesla <el_mallic@hotmail.com>
2023-07-23 19:23:25 -0300

    nueva app

--- Programacion/002 ejemplos/002 - 14 django/15 dijango mensages/carteles/settings.py
+++ Programacion/002 ejemplos/002 - 14 django/15 dijango mensages/carteles/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "mensajes"
+    "mensajes",
+    "url_parametro_opsional"
 ]
 
 MIDDLEWARE = [
