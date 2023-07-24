d328c7105f7c03a048f5d715cc26686c1734129f
Mallic Tesla <el_mallic@hotmail.com>
2023-07-23 21:03:37 -0300

    nueva app filtros

--- Programacion/002 ejemplos/002 - 14 django/15 dijango mensages/carteles/settings.py
+++ Programacion/002 ejemplos/002 - 14 django/15 dijango mensages/carteles/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "mensajes",
     "url_parametro_opsional",
+    "filtros",
 ]
 
 MIDDLEWARE = [
--- Programacion/002 ejemplos/002 - 14 django/15 dijango mensages/carteles/urls.py
+++ Programacion/002 ejemplos/002 - 14 django/15 dijango mensages/carteles/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("mensajes/", include ("mensajes.urls")),
-    path("url_parametro_opsional/", include ("url_parametro_opsional.urls")),
+    path ('admin/', admin.site.urls),
+    path ("mensajes/", include ("mensajes.urls")),
+    path ("url_parametro_opsional/", include ("url_parametro_opsional.urls")),
+    path ("filtros/", include ("filtros.urls"))
 ]
