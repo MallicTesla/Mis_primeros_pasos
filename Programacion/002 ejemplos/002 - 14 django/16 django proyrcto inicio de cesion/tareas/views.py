d8d3e57e7f23d574c898c972f377df2021273582
Mallic Tesla <el_mallic@hotmail.com>
2023-08-11 17:18:08 -0300

    crear usuario pronto

--- Programacion/002 ejemplos/002 - 14 django/16 django proyrcto inicio de cesion/tareas/views.py
+++ Programacion/002 ejemplos/002 - 14 django/16 django proyrcto inicio de cesion/tareas/views.py
@@ -9,21 +9,32 @@ def menu (request:HttpRequest):
     return render (request, "menu.html", {})
 
 def registro (request:HttpRequest):
+    mensaje = ""
+
     if request.method == "GET":
         print ("enviando formulario")
 
     else:
         #   para ver si la confirmasion de la contntraseña es corecta
         if request.POST["password1"] == request.POST["password2"]:
-            #   registrar usuario
-            User.objects.create_user(username = request.POST["use"])
+            #   la creasion de un usuario se coloca dentro de un try por si se crean 2 usuarios con el mismo nombre
+            try:
+                #   user es un objeto que contiene el usuario con la contraseña a guardar
+                user = User.objects.create_user(username = request.POST["username"], password = request.POST["password1"])
+                #   guarda el usuaario en la base de datos 
+                user.save()
+                mensaje = "Usuario creado"
 
-        return HttpRequest ("las contraseñas no son iguales")
+            except:
+                mensaje = "El usuario ya existe"
+
+        else:
+            mensaje = "Las contraseñas no son iguales"
 
         print (request.POST)
         print ("obtenienfo datos")
 
-    return render (request, "registro_form.html", {'form':UserCreationForm})
+    return render (request, "registro_form.html", {'form':UserCreationForm, "mensaje":mensaje})
 
 
 
