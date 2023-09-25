b0780348e90b220e6b371ac685ae418bbefe00fd
Mallic Tesla <el_mallic@hotmail.com>
2023-09-25 00:44:54 -0300

    por ahora pronto

--- Programacion/009-descargar videps/descargar.py
+++ Programacion/009-descargar videps/descargar.py
@@ -3,11 +3,11 @@ from moviepy.editor import VideoFileClip, AudioFileClip, clips_array
 
 def descarga (link, quiero, ruta_video, ruta_audio, final):
     lista = YouTube (link)
-    directo = lista.streams.get_highest_resolution()
-    print ("mas resolusion ", directo)
+    # directo = lista.streams.get_highest_resolution()
+    # print ("mas resolusion ", directo)
 
-    es_este = lista.streams.filter(res=quiero, mime_type="video/webm").order_by("resolution").desc().first()
-    print ("resolusion quiero ", es_este)
+    # es_este = lista.streams.filter(res=quiero, mime_type="video/webm").order_by("resolution").desc().first()
+    # print ("resolusion quiero ", es_este)
 
     videos = lista.streams.order_by("resolution").desc().filter (type = "video")
     audios = lista.streams.order_by("abr").desc().filter(type = "audio")
@@ -24,38 +24,37 @@ def descarga (link, quiero, ruta_video, ruta_audio, final):
 
     # print()
 
-    video_descargado = videos[0]
-    audio_descargado = audios[0]
+    video = videos[0]
+    audio = audios[0]
 
-
-    print (f"primer video {video_descargado}")
-    print (f"primer audio {audio_descargado}")
-
+    print (f"primer video {video}")
+    print (f"primer audio {audio}")
 
     try:
-        print ("descargo ",es_este)
-        video_descargado.download (ruta_video)
-        audio_descargado.download (ruta_audio)
+        video.download (ruta_video)
+        audio.download (ruta_audio)
 
         print ("descarga completa")
 
         try:
             print ("0")
-            video = VideoFileClip (f"{ruta_video}/{video_descargado.default_filename}")
+            video = VideoFileClip (f"{ruta_video}/{video.default_filename}")
             print ("1")
-            audio = AudioFileClip (f"{ruta_audio}/{audio_descargado.default_filename}")
+            audio = AudioFileClip (f"{ruta_audio}/{audio.default_filename}")
 
             print ("2")
             fusion = video.set_audio (audio)
             print ("3")
-            # video_with_audio.write_videofile(f"{ruta_descarga}/{yt.title}_final.mp4", codec="libx264")
-            fusion.write_videofile (f"{final}/{lista.title}_final.mp4", codec="libx264", audio_codec="aac")
+
+            # titulo = str (lista.title)
+            # print (f"({titulo})")
+            fusion.write_videofile (f"{final}\\final", codec="libx264", audio_codec="aac")
+
         except Exception as e:
             print ("Fallo la fusion", str(e))
     except:
         print ("fallo descarga")
-
-    print ("pronto")
+    print (f"Proseso terminado")
 
 
 link = "https://www.youtube.com/watch?v=8-bSHuiTSoM&ab_channel=DateunVlog"
