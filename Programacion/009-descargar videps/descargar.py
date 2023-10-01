379f761adae6b909f3d3205cf8da7add8c527c58
Mallic Tesla <el_mallic@hotmail.com>
2023-10-01 16:19:07 -0300

    V 1.1

--- Programacion/009-descargar videps/descargar.py
+++ Programacion/009-descargar videps/descargar.py
@@ -3,64 +3,63 @@ from moviepy.editor import VideoFileClip, AudioFileClip, clips_array
 
 def descarga (link, quiero, ruta_video, ruta_audio, final, lo_mejor, defecto):
     lista = YouTube (link)
-    # es_este = lista.streams.filter(res=quiero, mime_type="video/webm").order_by("resolution").desc().first()
-    # print ("resolusion quiero ", es_este)
+
+    video_completo = lista.streams.get_highest_resolution()
 
     videos = lista.streams.order_by("resolution").desc().filter (type = "video")
     audios = lista.streams.order_by("abr").desc().filter(type = "audio")
 
-    for video in videos :
-        print ("todas ",video)
-    print ()
-    for audio in audios :
-        print ("audios", audio)
+    # for video in videos :
+    #     print ("todas ",video)
+    # print ()
+    # for audio in audios :
+    #     print ("audios", audio)
 
     print()
 
-    if lo_mejor is True:
+    video = videos[0]
+    audio = audios[0]
+
+    print (f"video_completo.resolution {video_completo.resolution}")
+    print (f"primer video {video}")
+    # print (f"primer audio {audio}")
+
+    if defecto or video_completo.resolution is video.resolution:
+        print ("video_completo")
+        # try:
+        #     video_completo.download(final)
+
+        # except:
+        #     print ("fallo descarga")
+
+    else:
         print ("lo mejor")
-        video = videos[0]
-        audio = audios[0]
+        # try:
+        #     video.download (ruta_video)
+        #     audio.download (ruta_audio)
 
-        print (f"primer video {video}")
-        print (f"primer audio {audio}")
+        #     print ("descarga completa")
 
-        try:
-            video.download (ruta_video)
-            audio.download (ruta_audio)
+        #     try:
+        #         print ("1")
+        #         video = VideoFileClip (f"{ruta_video}/{video.default_filename}")
+        #         print ("2")
+        #         audio = AudioFileClip (f"{ruta_audio}/{audio.default_filename}")
+        #         print ("3")
 
-            print ("descarga completa")
+        #         fusion = video.set_audio (audio)
 
-            try:
-                print ("1")
-                video = VideoFileClip (f"{ruta_video}/{video.default_filename}")
-                print ("2")
-                audio = AudioFileClip (f"{ruta_audio}/{audio.default_filename}")
-                print ("3")
+        #         # titulo = str (lista.title)
+        #         # print (f"({titulo})")
+        #         print ("4")
+        #         fusion.write_videofile (f"{final}\\final.mp4", codec="libx264", audio_codec="aac")
+        #         print ("5")
 
-                fusion = video.set_audio (audio)
-
-                # titulo = str (lista.title)
-                # print (f"({titulo})")
-                print ("4")
-                fusion.write_videofile (f"{final}\\final.mp4", codec="libx264", audio_codec="aac")
-                print ("5")
-
-            except Exception as e:
-                print ("Fallo la fusion", str(e))
-        except:
-            print ("fallo descarga")
-        print (f"Proseso terminado")
-
-    elif defecto is True:
-        print ("defecto")
-        try:
-            defecto = lista.streams.get_highest_resolution()
-
-            defecto.download(final)
-        except:
-            print ("fallo descarga")
-
+        #     except Exception as e:
+        #         print ("Fallo la fusion", str(e))
+        # except:
+        #     print ("fallo descarga")
+        # print (f"Proseso terminado")
 
 
 
@@ -68,6 +67,8 @@ def descarga (link, quiero, ruta_video, ruta_audio, final, lo_mejor, defecto):
 
 # link = "https://www.youtube.com/watch?v=8-bSHuiTSoM&ab_channel=DateunVlog"
 link = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+# link = "https://www.youtube.com/watch?v=GVkiFP71n_c&ab_channel=LaMomiadelHugo"
+
 lo_mejor = True
 defecto = False
 quiero = "1080p"
--- Programacion/009-descargar videps/pruevas.py
+++ Programacion/009-descargar videps/pruevas.py
@@ -3,12 +3,31 @@ from pytube import YouTube
 def descarga (link):
     lista = YouTube (link)
 
-    comparar = lista.streams.get_highest_resolution()
+    video_completo = lista.streams.get_highest_resolution()
+
+    videos = lista.streams.order_by("resolution").desc().filter (type = "video")
+    audios = lista.streams.order_by("abr").desc().filter(type = "audio")
+
+    for video in videos :
+        print ("todos ",video)
+    print ()
+    print (video_completo)
+
+    print (video_completo.resolution)
+
+    video = videos[0]
+
+    if video_completo.resolution is video.resolution:
+        print ("si")
+    else:
+        print ("NO")
 
 
 
 
-link = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
 
+# link = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+link = "https://www.youtube.com/watch?v=GVkiFP71n_c&ab_channel=LaMomiadelHugo"
 
+descarga (link)
 
