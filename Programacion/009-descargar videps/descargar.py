68898b156c5edbe3306cf8a156c48d97ed1639f1
Mallic Tesla <el_mallic@hotmail.com>
2023-10-01 20:58:20 -0300

    V 0.2

--- Programacion/009-descargar videps/descargar.py
+++ Programacion/009-descargar videps/descargar.py
@@ -1,13 +1,15 @@
 from pytube import YouTube
 from moviepy.editor import VideoFileClip, AudioFileClip, clips_array
 
-def descarga (link, quiero, ruta_video, ruta_audio, final, lo_mejor, defecto):
+def descarga (link, quiero, ruta_video, ruta_audio, final, lo_mejor, descarga_rapida):
     lista = YouTube (link)
 
     video_completo = lista.streams.get_highest_resolution()
 
     videos = lista.streams.order_by("resolution").desc().filter (type = "video")
     audios = lista.streams.order_by("abr").desc().filter(type = "audio")
+
+    videos_quiero = videos.filter (resolution = quiero)
 
     # for video in videos :
     #     print ("todas ",video)
@@ -17,64 +19,102 @@ def descarga (link, quiero, ruta_video, ruta_audio, final, lo_mejor, defecto):
 
     print()
 
+    # video_quiero = videos_quiero [0]
     video = videos[0]
     audio = audios[0]
 
-    print (f"video_completo.resolution {video_completo.resolution}")
-    print (f"primer video {video}")
+    # print (f"video quiero {video_quiero}")
+    # print (f"video_completo.resolution {video_completo.resolution}")
+    # print (f"primer video {video}")
     # print (f"primer audio {audio}")
 
-    if defecto or video_completo.resolution is video.resolution:
+    if descarga_rapida or video_completo.resolution is video.resolution:
         print ("video_completo")
-        # try:
-        #     video_completo.download(final)
+        try:
+        #     # video_completo.download(final, filename = 'prueva')
+            video_completo.download(final)
 
-        # except:
-        #     print ("fallo descarga")
+        except:
+            print ("fallo descarga")
 
     else:
-        print ("lo mejor")
-        # try:
-        #     video.download (ruta_video)
-        #     audio.download (ruta_audio)
+        if lo_mejor and quiero == "":
+            print (f"lo mejor {video}")
+        else:
+            video = videos_quiero [0]
+            print (f"quiero {video}")
 
-        #     print ("descarga completa")
+        try:
+            video.download (ruta_video)
+            audio.download (ruta_audio)
 
-        #     try:
-        #         print ("1")
-        #         video = VideoFileClip (f"{ruta_video}/{video.default_filename}")
-        #         print ("2")
-        #         audio = AudioFileClip (f"{ruta_audio}/{audio.default_filename}")
-        #         print ("3")
+            print ("descarga completa")
 
-        #         fusion = video.set_audio (audio)
+            try:
+                video = VideoFileClip (f"{ruta_video}/{video.default_filename}")
+                audio = AudioFileClip (f"{ruta_audio}/{audio.default_filename}")
 
-        #         # titulo = str (lista.title)
-        #         # print (f"({titulo})")
-        #         print ("4")
-        #         fusion.write_videofile (f"{final}\\final.mp4", codec="libx264", audio_codec="aac")
-        #         print ("5")
+                fusion = video.set_audio (audio)
 
-        #     except Exception as e:
-        #         print ("Fallo la fusion", str(e))
-        # except:
-        #     print ("fallo descarga")
-        # print (f"Proseso terminado")
+                fusion.write_videofile (f"{final}\\final.mp4", codec="libx264", audio_codec="aac")
+
+            except Exception as e:
+                print ("Fallo la fusion", str(e))
+        except:
+            print ("fallo descarga")
+        print (f"Proseso terminado")
+    print ("Descarga completa")
+
+def descargar_audio (link_audio, ruta_audio):
+    lista = YouTube (link_audio)
+    audios = lista.streams.order_by("abr").desc().filter(type = "audio")
+
+    for audio in audios :
+        print (f"audio {audio}")
+
+    audio = audios[0]
+
+    audio.download (ruta_audio)
 
 
 
-
-
-# link = "https://www.youtube.com/watch?v=8-bSHuiTSoM&ab_channel=DateunVlog"
-link = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
-# link = "https://www.youtube.com/watch?v=GVkiFP71n_c&ab_channel=LaMomiadelHugo"
-
-lo_mejor = True
-defecto = False
-quiero = "1080p"
+# --------------------------------------------------------------------------------------------------------
+# RUTAS DE DESTINO
 
 ruta_video = "C:\\Users\\Mallic\\Downloads\\YouTube\\video"
 ruta_audio = "C:\\Users\\Mallic\\Downloads\\YouTube\\audio"
 final = "C:\\Users\\Mallic\\Downloads\\YouTube\\pronto"
 
-descarga (link, quiero, ruta_video, ruta_audio, final, lo_mejor, defecto)
+# --------------------------------------------------------------------------------------------------------
+# DESCARGAR AUDIO
+
+# link_audio = "https://www.youtube.com/watch?v=jEugr6x2_qc&ab_channel=Bizarro"
+link_audio = "https://www.youtube.com/watch?v=GVkiFP71n_c&ab_channel=LaMomiadelHugo"
+
+# descomenta esto para descargar audio y comenta el de DESCARGAR VIDEOS
+descargar_audio (link_audio, ruta_audio)
+
+# --------------------------------------------------------------------------------------------------------
+# DESCARGAR VIDEOS
+
+# link = "https://www.youtube.com/watch?v=8-bSHuiTSoM&ab_channel=DateunVlog" #1080
+# link = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" #1080
+link = "https://www.youtube.com/watch?v=GVkiFP71n_c&ab_channel=LaMomiadelHugo" #720
+# link = "https://www.youtube.com/watch?v=jEugr6x2_qc&ab_channel=Bizarro" # 2160
+
+lo_mejor = True
+quiero = ""
+
+descarga_rapida = lo_mejor is False and quiero == ""
+
+# descomenta esto para descargar videos y comenta el de DESCARGAR AUDIO
+# descarga (link, quiero, ruta_video, ruta_audio, final, lo_mejor, descarga_rapida)
+
+
+# --------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------
+
+
+
+
+
--- Programacion/009-descargar videps/pruevas.py
+++ Programacion/009-descargar videps/pruevas.py
@@ -1,17 +1,34 @@
 from pytube import YouTube
 
-def descarga (link):
+def descarga (link, quiero=""):
     lista = YouTube (link)
 
     video_completo = lista.streams.get_highest_resolution()
 
     videos = lista.streams.order_by("resolution").desc().filter (type = "video")
-    audios = lista.streams.order_by("abr").desc().filter(type = "audio")
+    audios = lista.streams.order_by("abr").desc().filter (type = "audio")
 
-    for video in videos :
-        print ("todos ",video)
+    video_quiero = videos.filter (resolution = quiero)
+
+    # for video in videos :
+    #     print ("todos ",video)
+
+    print()
+
+    # for audio in audios :
+    #     print (f"audios",audio)
+
+    # print()
+
+    # for video in videos :
+    #     if video.resolution == quiero:
+    #         print ("todos ",video)
+
+    for videos_quiero in video_quiero:
+        print (videos_quiero)
+
     print ()
-    print (video_completo)
+    print ("video_completo",video_quiero)
 
     print (video_completo.resolution)
 
@@ -22,12 +39,20 @@ def descarga (link):
     else:
         print ("NO")
 
+    if quiero != "" :
+        print ("tiene algo",quiero)
+    else:
+        print ("esta vacio")
 
 
 
 
+# link = "https://www.youtube.com/watch?v=8-bSHuiTSoM&ab_channel=DateunVlog"
 # link = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
-link = "https://www.youtube.com/watch?v=GVkiFP71n_c&ab_channel=LaMomiadelHugo"
+# link = "https://www.youtube.com/watch?v=GVkiFP71n_c&ab_channel=LaMomiadelHugo"
+link = "https://www.youtube.com/watch?v=jEugr6x2_qc&ab_channel=Bizarro"
 
-descarga (link)
+quiero = ""
+
+descarga (link, quiero)
 
