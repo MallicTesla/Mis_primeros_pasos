b50e301e40b8590d7a6c61355622310d1ad1643c
Mallic Tesla <el_mallic@hotmail.com>
2024-03-13 20:35:18 -0300

    funciones con javascript

---
+++ Programacion/009-JavaScript/01-funciones.js
@@ -0,0 +1,43 @@
+const esto = (valor) => {
+    console.log("Funcion: esto");
+
+    const esIgual = (otro) => {
+        console.log("Funcion: esIgual");
+
+        if (otro == valor){
+            return console.log ("exitoso :", otro == valor)
+
+        } else {
+            return console.log ("exitoso :", otro == valor, "causa: No es igual")
+        }
+    };
+
+    const noEsIgual = (otro) => {
+        console.log("Funcion: noEsIgual");
+
+        if (otro != valor){
+            return console.log ("exitoso :", otro != valor)
+
+        } else {
+            return console.log ("exitoso :", otro != valor, "causa: Es igual")
+        }
+    }
+
+    return {
+        esIgual,
+        noEsIgual,
+    };
+}
+
+/* 
+Cambiar resultado a un objeto.
+Posibles valores:
+    - { exitoso: true }
+    - { exitoso: false, causa: "No es igual" }
+    - { exitoso: false, causa: "Es igual" }
+*/
+esto(5).esIgual(5); //  { exitoso: true }
+esto(5).esIgual(10); // { exitoso: false, causa: "No es igual" }
+
+esto("Hola").noEsIgual("Chau"); // { exitoso: true }
+esto("Hola").noEsIgual("Hola"); // { exitoso: false, causa: "Es igual" }
