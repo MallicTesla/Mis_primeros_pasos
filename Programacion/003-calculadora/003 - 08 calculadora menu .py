0d2f8546ca879228e793e62a879cc9a2cc592c60
Mallic Tesla <el_mallic@hotmail.com>
2024-04-17 22:06:44 -0300

    algunos cambios

--- Programacion/003-calculadora/003 - 08 calculadora menu .py
+++ Programacion/003-calculadora/003 - 08 calculadora menu .py
@@ -14,7 +14,7 @@ def menu ():
 def calculo () :
     operasion = input ()
     if operasion == "+":
-        N1 = pedir_numero( "poneme el numero que quieras ")
+        N1 = pedir_numero ( "poneme el numero que quieras ")
         N2 = pedir_numero("pone el otro numero ")
         print ("elegistes + ")
         print (N1+N2)
