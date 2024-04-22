f8cdf16ac31c24a3c7d87a4383f882bebe143a39
Mallic Tesla <el_mallic@hotmail.com>
2024-04-21 23:05:24 -0300

    lo mismo

--- Programacion/002 ejemplos/002 - 18 Prints/002 - 01 Print diccionario.py
+++ Programacion/002 ejemplos/002 - 18 Prints/002 - 01 Print diccionario.py
@@ -17,7 +17,7 @@ album_set2 = set([ "AC/DC", "Back in Black", "The Dark Side of the Moon"])
 intersection = album_set1 & album_set2              # asi comparas dos set para ver los valores en comun
 
 print ( 4,intersection)
-print ( 5, album_set1.difference(album_set2))       # muestra las diferensias de la lista album_set1 con respecto de la lista album_set2
+print ( 5, album_set1.difference (album_set2))       # muestra las diferensias de la lista album_set1 con respecto de la lista album_set2
 print ( 6, album_set1.union(album_set2))            # une las dos listas sin que se repitan los valores
 
 print ( 7, set({"Back in Black", "AC/DC"}).issubset(album_set1))    #comprueva si un conjunto/set esta dentro de otro
